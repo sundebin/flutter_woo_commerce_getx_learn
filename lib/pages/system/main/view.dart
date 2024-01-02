b273e4eabaaafa26c1cdee044c8e8659ab200659
孙德彬 <sundebin68@gmail.com>
2024-01-02 15:24:56 +0800

    Revert "17.1 商品页加购物车"
    This reverts commit a7e19e707af23e3b542d1ff926e4b2843a1e170f.

--- lib/pages/system/main/view.dart
+++ lib/pages/system/main/view.dart
@@ -4,6 +4,8 @@ import 'package:flutter_woo_commerce_getx_learn/common/index.dart';
 import 'package:flutter_woo_commerce_getx_learn/pages/index.dart';
 import 'package:get/get.dart';
 
+import 'index.dart';
+
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
 
